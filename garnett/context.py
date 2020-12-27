08e8837f33435e4dbb2d7d73d20447932957b55e
Sam Spencer <theodore.therone@gmail.com>
2020-12-27 11:55:49 +1100

    more GH actions debug

--- garnett/context.py
+++ garnett/context.py
@@ -17,7 +17,7 @@ class set_field_language(ContextDecorator):
         pass
 
     def __exit__(self, exc_type, exc_value, traceback):
-        # Using Context.reset like shown below causes wierd errors
+        # Using Context.reset like shown below causes weird errors
         # TODO: Maybe fix this?
         # ctx_language.reset(self.language)
         ctx_language.set(self.old_language)
