aab31b26f97ad03ac3c79487302d23b45d8f3d15
Harry White <harry.white424@gmail.com>
2021-01-22 14:30:11 +1100

    Only use the not found middleware in tests
    since it mirrors the TranslationContextMiddleware functionality anyways

--- tests/library_app/settings.py
+++ tests/library_app/settings.py
@@ -53,7 +53,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "garnett.middleware.TranslationContextMiddleware",
     "garnett.middleware.TranslationContextNotFoundMiddleware",
 ]
 
