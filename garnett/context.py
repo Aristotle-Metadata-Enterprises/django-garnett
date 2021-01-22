66beedae59e0c015ed297bf28722b5b26be5cc7d
Harry White <harry.white424@gmail.com>
2021-01-22 11:10:41 +1100

    Make decorator use context reset
    get_current_language now returns default if it needs to which will make
    changing that to be a callable setting easy

--- garnett/context.py
+++ garnett/context.py
@@ -1,23 +1,16 @@
 from contextlib import ContextDecorator
 import contextvars
 
-from garnett.utils import get_default_language
-
 ctx_language = contextvars.ContextVar("garnett_language")
 
 
 class set_field_language(ContextDecorator):
-    def __init__(self, language, deactivate=False):
-        self.old_language = ctx_language.set(language).old_value
-        if self.old_language == contextvars.Token.MISSING:
-            self.old_language = get_default_language()
-        self.deactivate = deactivate
+    def __init__(self, language):
+        self.language = language
+        self.token = None
 
     def __enter__(self):
-        pass
+        self.token = ctx_language.set(self.language)
 
     def __exit__(self, exc_type, exc_value, traceback):
-        # Using Context.reset like shown below causes weird errors
-        # TODO: Maybe fix this?
-        # ctx_language.reset(self.language)
-        ctx_language.set(self.old_language)
+        ctx_language.reset(self.token)
